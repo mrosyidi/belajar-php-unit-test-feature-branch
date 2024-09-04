bb07eb6abb08511e97733070da666dbc25e04969
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-04 22:46:17 +0700

    update MathTest.php unit test dengan data provider

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -14,4 +14,23 @@
       self::assertEquals(0, Math::sum([]));
       self::assertEquals(2, Math::sum([2]));
     }
+
+    /**
+    * @dataProvider mathSumData
+    */
+    public function testDataProvider(array $values, int $expected)
+    {
+      self::assertEquals($expected, Math::sum($values));
+    }
+
+    public function mathSumData(): array
+    {
+      return [
+        [[5, 5], 10],
+        [[4, 4, 4, 4, 4], 20],
+        [[3, 3, 3], 9],
+        [[], 0],
+        [[2], 2]
+      ];
+    }
   }
