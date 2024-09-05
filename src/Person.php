d24664153edbe4fcd16a56fef27708ce667d8da6
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-05 09:24:07 +0700

    update Person.php test output

--- src/Person.php
+++ src/Person.php
@@ -14,4 +14,9 @@
       if($name == null) throw new \Exception('Name is null');
       return "Hello $name, my name is $this->name";
     }
+
+    public function sayGoodBye(?string $name): void
+    {
+      echo "Good bye $name" . PHP_EOL;
+    }
   }
