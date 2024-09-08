def78484826fc8ab6ba70144df78fc2aa37ec430
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-08 20:07:34 +0700

    update ProductServiceTest.php konfigurasi stub dengan map

--- tests/ProductServiceTest.php
+++ tests/ProductServiceTest.php
@@ -23,4 +23,19 @@
       $result = $this->repository->findById("1");
       self::assertSame($product, $result);
     }
+
+    public function testStubMap()
+    {
+      $product1 = new Product();
+      $product1->setId("1");
+      $product2 = new Product();
+      $product2->setId("2");
+      $map = [
+        ["1", $product1],
+        ["2", $product2]
+      ];
+      $this->repository->method("findById")->willReturnMap($map);
+      self::assertSame($product1, $this->repository->findById("1"));
+      self::assertSame($product2, $this->repository->findById("2"));
+    }
   }
