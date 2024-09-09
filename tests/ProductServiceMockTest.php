18f4b1574c5c2a3bf58ebea1aca16fee6c713e60
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-09 08:39:56 +0700

    update ProductServieMockTest.php konfigurasi mock objek

--- tests/ProductServiceMockTest.php
+++ tests/ProductServiceMockTest.php
@@ -88,4 +88,13 @@
       $this->repository->method("findById")->willReturn(null);
       $this->service->delete("1");
     }
+
+    public function testMock()
+    {
+      $product = new Product();
+      $product->setId("1");
+      $this->repository->expects($this->once())->method("findById")->willReturn($product);
+      $result = $this->repository->findById("1");
+      self::assertSame($product, $result);
+    }
   }
